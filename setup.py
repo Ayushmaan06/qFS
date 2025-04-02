1652f7d74ba86cfe1a96de59348b4fbd69fbcc2e
ayushmaan <s.ayushmaan06@gmail.com>
2025-04-02 15:55:25 +0530

    Changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qFS",
-    version="0.0.1",
+    version="0.0.2",
     description="A Python package for Quantum Feature Selection (qFS).",
     author="Ayushmaan Singh",
     packages=find_packages(),
